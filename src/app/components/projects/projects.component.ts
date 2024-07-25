aa8264c33f899825130315901823050719f2bd91
Arunabha-NOTE <arunabham2017@gmail.com>
2024-07-25 19:26:06 +0530

    added file for search console

---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import { ProjectModel } from "../../models/projectModel"
+import projectData from "../../data/projectData";
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css'
+})
+export class ProjectsComponent {
+  projects:  ProjectModel[] = projectData;
+}
