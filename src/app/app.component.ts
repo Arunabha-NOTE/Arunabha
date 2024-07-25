f14467b53d85a22519e93f0e482c64cc43125276
Arunabha-NOTE <arunabham2017@gmail.com>
2024-07-25 18:07:16 +0530

    added progressbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FooterComponent} from "./components/footer/footer.component";
 import {HeaderComponent} from "./components/header/header.component";
+import {ProgressBarComponent} from "./components/progress-bar/progress-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, ProgressBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
