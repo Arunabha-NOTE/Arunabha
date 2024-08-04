ec1f175a351345b5035c5525a1affc3330206151
Arunabha-NOTE <arunabham2017@gmail.com>
2024-08-04 13:31:00 +0530

    added project

--- src/app/data/projectData.ts
+++ src/app/data/projectData.ts
@@ -1,36 +1,44 @@
 import {ProjectModel} from "../models/projectModel";
 
-const projectData: ProjectModel[] = [{
-  id: 1,
-  name: "Synthetic Data Generator",
-  description: "This is a web application where you can upload sample data and configure options to use a large language model (LLM) to generate synthetic data.",
-  link: "https://github.com/Arunabha-NOTE/synthetic-data-gen-ui",
-  platform: "Github"
-}, {
-  id: 2,
-  name: "Startup Weekend Jaipur 2024",
-  description: "This is the official website for startup weekend jaipur 2024. It is a 3 day event where you can pitch your ideas, form teams and build a startup in 54 hours.",
-  link: "https://github.com/pujan-modha/swj-24",
-  platform: "Github"
-}, {
-  id: 3,
-  name: "To-Do Application",
-  description: "This is a web application where you can add lists and custom to-dos for efficient work management. It is built using multiple technologies such as Spring Boot, JSP, and React.",
-  link: "https://github.com/Arunabha-NOTE/ToDo-App",
-  platform: "Github"
-}, {
-  id: 4,
-  name: "IEEE CS MUJ",
-  description: "This is the official website for IEEE CS MUJ. A student branch of IEEE Computer Society at Manipal University Jaipur.",
-  link: "https://github.com/IEEECSMUJ/IEEECSMUJ",
-  platform: "Github"
-}, {
-  id: 5,
-  name: "IC Hack 2.0",
-  description: "This is the official website for IC Hack 2.0. A annual hackathon conducted by the IEEE India Council.",
-  link: "https://github.com/pujan-modha/ichack23",
-  platform: "Github"
-}
+const projectData: ProjectModel[] = [
+  {
+    id: 1,
+    name: "Devsphere",
+    description: "A SaaS for easy provisioning and deployment of workspaces through our cli or ui in various providers like AWS, GCP, Azure, etc. It is built using multiple technologies such as Spring Boot, react, Terraform, CircleCI, docker, etc.",
+    link: "https://gitlab.com/devsphere1",
+    platform: "Gitlab"
+  },
+  {
+    id: 2,
+    name: "Synthetic Data Generator",
+    description: "This is a web application where you can upload sample data and configure options to use a large language model (LLM) to generate synthetic data.",
+    link: "https://github.com/Arunabha-NOTE/synthetic-data-gen-ui",
+    platform: "Github"
+  }, {
+    id: 3,
+    name: "Startup Weekend Jaipur 2024",
+    description: "This is the official website for startup weekend jaipur 2024. It is a 3 day event where you can pitch your ideas, form teams and build a startup in 54 hours.",
+    link: "https://github.com/pujan-modha/swj-24",
+    platform: "Github"
+  }, {
+    id: 4,
+    name: "To-Do Application",
+    description: "This is a web application where you can add lists and custom to-dos for efficient work management. It is built using multiple technologies such as Spring Boot, JSP, and React.",
+    link: "https://github.com/Arunabha-NOTE/ToDo-App",
+    platform: "Github"
+  }, {
+    id: 5,
+    name: "IEEE CS MUJ",
+    description: "This is the official website for IEEE CS MUJ. A student branch of IEEE Computer Society at Manipal University Jaipur.",
+    link: "https://github.com/IEEECSMUJ/IEEECSMUJ",
+    platform: "Github"
+  }, {
+    id: 6,
+    name: "IC Hack 2.0",
+    description: "This is the official website for IC Hack 2.0. A annual hackathon conducted by the IEEE India Council.",
+    link: "https://github.com/pujan-modha/ichack23",
+    platform: "Github"
+  }
 ]
 
 export default projectData;
