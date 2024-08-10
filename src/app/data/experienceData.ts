4da3c526aca49d6d820a591510a84b1dd46c778c
Arunabha-NOTE <arunabham2017@gmail.com>
2024-08-11 00:28:09 +0530

    updated links

--- src/app/data/experienceData.ts
+++ src/app/data/experienceData.ts
@@ -5,7 +5,7 @@ const experienceData: ExperienceModel[] = [
     id: 1,
     name: "Ericsson Intern",
     description: "During my internship at Ericsson, I contributed to an open-source project called Synthetic Data Generator UI/API. In the second half, I recreated the project using company tools with a tech stack including Angular, TypeScript, Less CSS, Python, FastAPI, SQLiteDB3, Git, and Gitlab.",
-    link: "https://www.linkedin.com/company/ericsson/",
+    link: "https://www.ericsson.com/en/about-us/company-facts/ericsson-worldwide/india",
     platform: "Link"
   }
 ]
