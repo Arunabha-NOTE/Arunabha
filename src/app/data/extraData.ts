4fad264d33e823719c40b958b641f1b282251981
Arunabha-NOTE <arunabham2017@gmail.com>
2024-11-05 13:03:24 +0530

    re-enabled button and added alternative links

--- src/app/data/extraData.ts
+++ src/app/data/extraData.ts
@@ -4,25 +4,25 @@ const ExtraData: ExtraModel[] = [{
   id: 1,
   name: "Startup Weekend Jaipur (June 2024 – Present)",
   description: "Co-lead Technical Team(EC Panel)",
-  link: "https://github.com/pujan-modha/swj-24",
-  platform: "Github"
+  link: "https://www.startupweekendjaipur.in/",
+  platform: "Website"
 }, {
   id: 2,
   name: "IEEE CS MUJ (March 2023 – April 2024)",
   description: "Senior Coordinator(Technical Team/Programs/Community",
-  link: "https://cs.ieeemuj.com/",
+  link: "https://ieeecsmuj.vercel.app/",
   platform: "Website"
 }, {
   id: 3,
   name: "MUJ HackX (September 2023 – October 2023)",
   description: "Events Working Team",
-  link: "https://www.mujhackx.com/",
+  link: "https://hack-xwebsite.vercel.app/",
   platform: "Website"
 }, {
   id: 4,
   name: "IC Hack2.0 (September 2023 – October 2023)",
   description: "Technical Team",
-  link: "https://ichack2023.ieeemuj.com/",
+  link: "https://ichack23.vercel.app/",
   platform: "Website"
 }
 ]
