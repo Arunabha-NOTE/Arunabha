d5053d22ab6ac1315566b81298164ea00421fa5b
Arunabha-NOTE <arunabham2017@gmail.com>
2025-03-27 20:35:49 +0530

    made changes to extracurricular data

--- src/app/data/extraData.ts
+++ src/app/data/extraData.ts
@@ -1,25 +1,37 @@
 import {ExtraModel} from "../models/extraModel";
 
 const ExtraData: ExtraModel[] = [{
-  id: 1,
-  name: "Startup Weekend Jaipur (June 2024 – Present)",
+    id: 1,
+    name: "Oneiros (January 2025 – March 2025)",
+    description: "Webmaster",
+    link: "https://www.oneirosmanipal.com/",
+    platform: "Website"
+},{
+    id: 2,
+    name: "Tedx Muj (October 2024 – November 2024)",
+    description: "Technical Director",
+    link: "https://www.tedxmanipaluniversityjaipur.com/",
+    platform: "Website"
+},{
+  id: 3,
+  name: "Startup Weekend Jaipur (June 2024 – October 2024)",
   description: "Co-lead Technical Team(EC Panel)",
   link: "https://www.startupweekendjaipur.in/",
   platform: "Website"
 }, {
-  id: 2,
+  id: 4,
   name: "IEEE CS MUJ (March 2023 – April 2024)",
   description: "Senior Coordinator(Technical Team/Programs/Community",
   link: "https://ieeecsmuj.vercel.app/",
   platform: "Website"
 }, {
-  id: 3,
+  id: 5,
   name: "MUJ HackX (September 2023 – October 2023)",
   description: "Events Working Team",
   link: "https://hack-xwebsite.vercel.app/",
   platform: "Website"
 }, {
-  id: 4,
+  id: 6,
   name: "IC Hack2.0 (September 2023 – October 2023)",
   description: "Technical Team",
   link: "https://ichack23.vercel.app/",
