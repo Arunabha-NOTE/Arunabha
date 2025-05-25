bdade8641b9eb7559dc23aca3970139e8dba26e1
Arunabha-NOTE <arunabham2017@gmail.com>
2025-05-26 01:32:36 +0530

    added lenis smooth scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, AfterViewInit, OnDestroy} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FooterComponent} from "./components/footer/footer.component";
 import {HeaderComponent} from "./components/header/header.component";
 import {ProgressBarComponent} from "./components/progress-bar/progress-bar.component";
+import Lenis from 'lenis';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,24 @@ import {ProgressBarComponent} from "./components/progress-bar/progress-bar.compo
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
+    private lenis: Lenis | undefined;
+
+    ngAfterViewInit(): void {
+        this.lenis = new Lenis();
+
+        const raf = (time: number) => {
+            // @ts-ignore
+            this.lenis.raf(time);
+            requestAnimationFrame(raf);
+        };
+        requestAnimationFrame(raf);
+    }
+
+    ngOnDestroy(): void {
+        if (this.lenis) {
+            this.lenis.destroy();
+        }
+    }
   title = 'Arunabha Mukhopadhyay';
 }
