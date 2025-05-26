602eaed2b024b496b9ce6ba57c14bd3f8d890593
Arunabha-NOTE <arunabham2017@gmail.com>
2025-05-26 13:51:15 +0530

    added animations to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {slideInFromBottomInitial} from "../../animations/initial-load.animation";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    imports: [
+        NgOptimizedImage
+    ],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    animations: [
+        slideInFromBottomInitial // Register the animation trigger
+    ]
 })
-export class HeaderComponent {
-  ScrollIntoView(elem: string) {
-    document.querySelector(elem)!.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
+export class HeaderComponent implements OnInit {
+    ngOnInit(): void {
+
+    }
+    ScrollIntoView(elem: string) {
+        document.querySelector(elem)!.scrollIntoView({behavior: 'smooth', block: 'start'});
+    }
+
+
 }
