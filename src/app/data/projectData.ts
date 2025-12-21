fd83ee9ed43d33e5d3113dd5f8ed6437a6b8e3fe
Arunabha Mukhopadhyay <arunabham2017@gmail.com>
2025-12-21 13:25:18 +0530

    Correct platform name from 'Github' to 'GitHub'

--- src/app/data/projectData.ts
+++ src/app/data/projectData.ts
@@ -7,42 +7,42 @@ const projectData: ProjectModel[] = [
         stack: "Angular, Typescript, Less CSS, FastAPI, Pandas, SQLite",
         description: "This is a web application where you can upload sample data and configure options to use a large language model (LLM) to generate synthetic data.",
         link: "https://github.com/Arunabha-NOTE/synthetic-data-gen-ui",
-        platform: "Github"
+        platform: "GitHub"
     }, {
         id: 2,
         name: "Startup Weekend Jaipur 2024",
         stack: "React, TypeScript, Tailwind CSS, MongoDB, Express",
         description: "This is the official website for startup weekend jaipur 2024.",
         link: "https://github.com/pujan-modha/swj-24",
-        platform: "Github"
+        platform: "GitHub"
     }, {
         id: 3,
         name: "To-Do Application",
         stack: "Spring Boot, MySQL, JSP, React, Tailwind CSS, JavaScript, Java",
         description: "This is a web application where you can add lists and custom to-dos for efficient work.",
         link: "https://github.com/Arunabha-NOTE/ToDo-App",
-        platform: "Github"
+        platform: "GitHub"
     }, {
         id: 4,
         name: "IEEE CS MUJ Website",
         stack: "NextJs, TypeScript, Tailwind CSS",
         description: "This is the official website for IEEE CS MUJ. A student branch of IEEE Computer Society at Manipal University Jaipur.",
         link: "https://github.com/IEEECSMUJ/IEEECSMUJ",
-        platform: "Github"
+        platform: "GitHub"
     }, {
         id: 5,
         name: "IC Hack 2.0 Website",
         stack: "React, Javascript, Tailwind CSS",
         description: "This is the official website for IC Hack 2.0. A annual hackathon conducted by the IEEE India Council.",
         link: "https://github.com/pujan-modha/ichack23",
-        platform: "Github"
+        platform: "GitHub"
     }, {
         id: 6,
         name: "DoIC MUJ Website ",
         stack: "React, TypeScript, Prismic, Tailwind CSS",
         description: "This is the official website for DoIC MUJ.",
         link: "https://github.com/doicmanipal/website",
-        platform: "Github"
+        platform: "GitHub"
     }, {
         id: 7,
         name: "CrowdSync ",
@@ -57,7 +57,7 @@ const projectData: ProjectModel[] = [
         stack: "NextJs, TypeScript, Tailwind CSS",
         description: "This is the official website for TedxMUJ.",
         link: "https://github.com/anvit-dd/tedx-muj",
-        platform: "Github"
+        platform: "GitHub"
     }, {
         id: 9,
         name: "Oneiros Muj 2024 website",
@@ -71,7 +71,7 @@ const projectData: ProjectModel[] = [
         stack: "Angular, Tailwind CSS, Spring Boot, TypeScript, Java, PostgreSQL (Supabase)",
         description: "A website to create and make use of Checklists for Flight simulation purposes.",
         link: "https://github.com/Arunabha-NOTE/checklist",
-        platform: "Github"
+        platform: "GitHub"
     }
     , {
         id: 11,
@@ -92,15 +92,15 @@ const projectData: ProjectModel[] = [
         name: "Muj ERP",
         stack: "NextJs, Tailwind CSS, TurboRepo, TypeScript, Prisma, PostgreSQL (Supabase), AWS, Docker",
         description: "A web based ERP system for Manipal University Jaipur.",
-        link: "",
-        platform: "NA"
+        link: "https://github.com/ERP-MUJ/ERP-SYS",
+        platform: "GitHub"
     },{
         id: 14,
         name: "Devsphere",
         stack: "Spring Boot, React, Terraform, CircleCI, Docker",
         description: "A platform for easy provisioning and deployment of configured virtual code spaces through our cli or ui in various providers like AWS, GCP, Azure, etc.",
         link: "https://gitlab.com/platinum-open-source/devsphere",
-        platform: "Gitlab"
+        platform: "GitLab"
     }
 ]
 
